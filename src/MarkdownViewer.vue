e3d0d7cf39f44873d7f5365f3ea9c5836c530ac6
huoshicang <2717334980@qq.com>
2025-09-26 17:26:50 +0800

    feat(:): 增加markdown的渲染
    增加markdown渲染组件，实现双向绑定
    
    Signed-off-by: huoshicang <2717334980@qq.com>

--- src/MarkdownViewer.vue
+++ src/MarkdownViewer.vue
@@ -1,27 +1,190 @@
-<script setup lang="ts">
-import { ref } from "vue";
+<template>
+  <div class="text-black" :class="wrapClass">
+    <div ref="textRef" class="leading-relaxed break-words">
+      <div>
+        <div v-if="!isText" class="markdown-body" v-html="text" />
+        <div v-else class="whitespace-pre-wrap" v-text="text" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, onMounted, onUnmounted, onUpdated, ref, defineProps } from "vue";
 import MarkdownIt from "markdown-it";
+import MdKatex from "@vscode/markdown-it-katex";
+import MdLinkAttributes from "markdown-it-link-attributes";
+import MdMermaid from "mermaid-it-markdown";
 import hljs from "highlight.js";
+import { useBasicLayout } from "../utils/useBasicLayout.ts";
+import { copyToClip } from "../utils/copy.ts";
 
-const props = defineProps<{
-  content: string;
-}>();
 
-// ✅ 给 md 加上类型
-const md: MarkdownIt = new MarkdownIt({
-  highlight(code: string, lang: string): string {
-    if (lang && hljs.getLanguage(lang)) {
-      try {
-        return hljs.highlight(code, { language: lang }).value;
-      } catch (__) {
-        return code;
-      }
+
+const props = defineProps({
+  // 文本内容
+  text: {
+    type: String,
+    default: "",
+  },
+  
+  // 是否为纯文本
+  isText: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const { isMobile } = useBasicLayout();
+
+const textRef = ref<HTMLElement>();
+
+/*
+* 高亮块
+* @param str 文本
+* @param lang 语言
+* */
+const highlightBlock = (str: string, lang?: string) => {
+  return `<pre class="code-block-wrapper">
+    <div class="code-block-header">
+        <span class="code-block-header__copy">复制代码</span>
+        <span class="code-block-header__lang">${lang}</span>
+    </div>
+    <code class="hljs code-block-body ${lang}">${str}</code>
+</pre>`;
+}
+
+
+const mdi = new MarkdownIt({
+  html: false,
+  linkify: true,
+  highlight(code, language) {
+    const validLang = !!(language && hljs.getLanguage(language));
+    if (validLang) {
+      const lang = language ?? "";
+      return highlightBlock(
+        hljs.highlight(code, { language: lang }).value,
+        lang,
+      );
     }
-    return code;
+    return highlightBlock(hljs.highlightAuto(code).value, "");
+  },
+});
+
+mdi
+  .use(MdLinkAttributes, { attrs: { target: "_blank", rel: "noopener" } })
+  .use(MdKatex)
+  .use(MdMermaid);
+
+const wrapClass = computed(() => {
+  return [
+    "text-wrap",
+    "min-w-[20px]",
+    "rounded-md",
+    isMobile.value ? "p-2" : "px-3 py-2",
+  ];
+});
+
+const text = computed(() => {
+  const value = props.text ?? "";
+  if (!props.isText) {
+    // 对数学公式进行处理，自动添加 $$ 符号
+    const escapedText = escapeBrackets(escapeDollarNumber(value));
+    return mdi.render(escapedText);
   }
+  return value;
+});
+
+
+/*
+* 添加复制事件
+* */
+const addCopyEvents = () => {
+  if (textRef.value) {
+    const copyBtn = textRef.value.querySelectorAll(".code-block-header__copy");
+    copyBtn.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const code = btn.parentElement?.nextElementSibling?.textContent;
+        if (code) {
+          copyToClip(code).then(() => {
+            btn.textContent = "复制成功";
+            setTimeout(() => {
+              btn.textContent = "复制代码";
+            }, 1000);
+          });
+        }
+      });
+    });
+  }
+}
+
+/*
+* 移除复制事件
+* */
+function removeCopyEvents() {
+  if (textRef.value) {
+    const copyBtn = textRef.value.querySelectorAll(".code-block-header__copy");
+    copyBtn.forEach((btn) => {
+      btn.removeEventListener("click", () => {});
+    });
+  }
+}
+
+// 数学公式
+function escapeDollarNumber(text: string) {
+  let escapedText = "";
+  
+  for (let i = 0; i < text.length; i += 1) {
+    let char = text[i];
+    const nextChar = text[i + 1] || " ";
+    
+    if (char === "$" && nextChar >= "0" && nextChar <= "9") char = "\\$";
+    
+    escapedText += char;
+  }
+  
+  return escapedText;
+}
+
+function escapeBrackets(text: string) {
+  const pattern =
+    /(```[\s\S]*?```|`.*?`)|\\\[([\s\S]*?[^\\])\\\]|\\\((.*?)\\\)/g;
+  return text.replace(
+    pattern,
+    (match, codeBlock, squareBracket, roundBracket) => {
+      if (codeBlock) return codeBlock;
+      else if (squareBracket) return `$$${squareBracket}$$`;
+      else if (roundBracket) return `$${roundBracket}$`;
+      return match;
+    },
+  );
+}
+
+onMounted(() => {
+  addCopyEvents();
+});
+
+onUpdated(() => {
+  addCopyEvents();
+});
+
+onUnmounted(() => {
+  removeCopyEvents();
 });
 </script>
 
-<template>
-  <div v-html="md.render(props.content)"></div>
-</template>
+<style lang="less">
+@import url(../assets/style);
+
+.code-block-header__copy {
+  display: none;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+  margin-right: 10px;
+}
+
+.code-block-wrapper:hover .code-block-header__copy {
+  display: inline-block;
+  opacity: 1;
+}
+</style>
