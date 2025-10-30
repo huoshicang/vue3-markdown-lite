ea536e4362fa0fceb73cbc146e18dc7efad82f25
huoshicang <2717334980@qq.com>
2025-10-30 09:48:46 +0800

    🏗️buildvitepress打包后样式丢失
    Signed-off-by: huoshicang <2717334980@qq.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,4 @@
 import {defineConfig} from "vitepress";
-import path from "path";
 
 const isProd = process.env.VITE_MY_ENV
 
@@ -7,19 +6,6 @@ export default defineConfig({
   title: "Vue3 Markdown Lite",
   description: "轻量级 Vue3 Markdown 展示组件",
   base: isProd ? '/vue3-markdown-lite/' : '/',
-  vite: {
-    ssr: {
-      noExternal: true,
-      target: 'webworker' // 强制使用Web Worker环境而不是Node.js
-    },
-    resolve: {
-      alias: {
-        // 关键：将 `@` 别名指向项目根目录下的 `src` 目录
-        // 注意路径计算：从 docs/.vitepress 到 src 的相对路径是 `../../src`
-        '@': path.resolve(__dirname, '../../src')
-      }
-    }
-  },
   themeConfig: {
     nav: [
       {text: "指南", link: "/guide/"},
